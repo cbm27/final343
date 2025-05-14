c30bac301a6c9a3a3f0ab82427bc1ff534570589
cbm27 <baomei.cui27@qmail.cuny.edu>
2025-05-14 12:36:55 -0400

    added links

--- src/app/components/Menubar.jsx
+++ src/app/components/Menubar.jsx
@@ -1,11 +1,18 @@
+import Link from 'next/link'
 export default function Menubar
 () {
   return (
    <>
      <ul>
-      <li>Home</li>
-      <li>Midterm Part I</li>
-      <li>Midterm Part II</li>
+      <li>
+        <Link href='/'>Home</Link>
+      </li>
+      <li>
+        <a href='https://vercel.com/baomei-cuis-projects/exam1/78WJezFkAeQfh236JJxj7ik4EXe2' target='_blank'>Midterm Part I - Exam1 </a>
+      </li>
+       <li>
+       <a href='https://vercel.com/baomei-cuis-projects'target='_blank'>Midterm Part II site</a>
+        </li>
      </ul>
    </> 
  
